c9df9b9e687249b5165ea0516131b57cdce28515
jirafay <jirafay@crimson.ua.edu>
2024-02-06 00:59:26 -0600

    final

---
+++ Program.cs
@@ -0,0 +1,16 @@
+﻿namespace SP2024_Assignment1_jirafay;
+
+public class Program
+{
+    public static void Main(string[] args)
+    {
+        var builder = WebApplication.CreateBuilder(args);
+        var app = builder.Build();
+
+        app.UseDefaultFiles();
+        app.UseStaticFiles();
+
+        app.Run();
+    }
+}
+
